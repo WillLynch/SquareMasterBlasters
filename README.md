3d25da4b6a249a4d6bb3d202725b7ea1ecd0d16c
William Lynch <w4lynch@gmail.com>
2015-09-30 16:42:34 -0700

    Grid now selects one tile at a time, and got rid of kivy stuff

--- README.md
+++ README.md
@@ -4,17 +4,13 @@ SENG 330 Group 10 Project Repository. Turn-based 2-player game - name to be chos
 Setup:
 ------
 
-We will be using Python 2.7, and Kivy 1.9.0. Follow download/setup instructions at http://kivy.org/#download.
-Make sure you have the proper versions.
+We will be using Python 2.7, and PyGame. Follow download/setup instructions at http://www.pygame.org/hifi.html.
 
 I have personally chosen to develop on Linux, and use virtualenv to create an isolated environment for this
 specific project. You can read about virtualenv here: http://docs.python-guide.org/en/latest/dev/virtualenvs/.
-But virtualenv is not necessary, and i'm sure you will be able to get it running on Windows/Mac, as Kivy is
-intended to be cross-platform.
+But virtualenv is not necessary, and the project should work on Mac Windows or Linux.
 
-Once you have cloned this project and have Kivy installed, try running Game.py with the Python interpreter.
-You should see a window pop-up with a big button that says 'Hello World'. Let me know if you have any issues.
-
+Once you have cloned this project and have PyGame installed, try running game.py with the Python 2.7 interpreter.
 
 -Will
 
--- game.py
+++ game.py
@@ -3,17 +3,20 @@ from pygame.locals import *
 
 # pygame constants
 SQUARE_SIZE = 50
-
+CONTROL_PANEL_WIDTH = 400
 VERT_SQUARES = 16
 HOR_SQUARES = 16
-WINDOW_HEIGHT = SQUARE_SIZE*VERT_SQUARES
-WINDOW_WIDTH = SQUARE_SIZE*HOR_SQUARES
+WINDOW_WIDTH = SQUARE_SIZE*VERT_SQUARES
+WINDOW_HEIGHT = SQUARE_SIZE*HOR_SQUARES + CONTROL_PANEL_WIDTH
 
 BLACK = (0,0,0)
 WHITE = (255, 255, 255)
 GREY = (127,127,127)
+GREEN_TRANSPARENT = (0, 255, 0, 128)
+GRID_LINE_THICKNESS = 2
 
-class GridTile():
+class GridTile:
+
     def __init__(self,x,y):
         self.x = x
         self.y = y
@@ -22,22 +25,27 @@ class GridTile():
     def select(self):
         self.selected = True
 
+    def deselect(self):
+        self.selected = False
+
     def draw(self, windowSurface):
         X = self.x*SQUARE_SIZE+1
         Y = self.y*SQUARE_SIZE+1
+        self.draw_border(windowSurface)
         if self.selected:
-			pygame.draw.rect(windowSurface, BLACK, (X,Y,SQUARE_SIZE-1,SQUARE_SIZE-1))
+            pygame.draw.rect(windowSurface, GREEN_TRANSPARENT, (X,Y,SQUARE_SIZE-1,SQUARE_SIZE-1))
+        else:
+            pygame.draw.rect(windowSurface, WHITE, (X,Y,SQUARE_SIZE-1,SQUARE_SIZE-1))
+
+    def draw_border(self, windowSurface):
+        pygame.draw.lines(windowSurface, GREY, True, [(self.x*SQUARE_SIZE,self.y*SQUARE_SIZE), (self.x*SQUARE_SIZE+SQUARE_SIZE,self.y*SQUARE_SIZE)
+            , (self.x*SQUARE_SIZE+SQUARE_SIZE,self.y*SQUARE_SIZE+SQUARE_SIZE), (self.x*SQUARE_SIZE,self.y*SQUARE_SIZE+SQUARE_SIZE)], GRID_LINE_THICKNESS)
 
 
-class Grid():
+class Grid:
     def __init__(self):
         self.grid = {(x, y): GridTile(x,y) for x in range(HOR_SQUARES) for y in range(VERT_SQUARES)}
-
-    def draw_grid_lines(self, windowSurface):
-        for x in range(0,WINDOW_HEIGHT,SQUARE_SIZE):
-            pygame.draw.line(windowSurface, GREY, [x,0], [x,WINDOW_WIDTH], 2)
-        for y in range(0,WINDOW_WIDTH,SQUARE_SIZE):
-            pygame.draw.line(windowSurface,GREY,[0,y], [WINDOW_HEIGHT,y], 2)
+        self.current_tile = None
 
     def draw(self, windowSurface):
         for x in range(HOR_SQUARES):
@@ -45,11 +53,13 @@ class Grid():
                 self.grid[(x, y)].draw(windowSurface)
 
     def select_tile(self, coordinates):
-        self.grid[coordinates].select()
+        if self.current_tile:
+            self.current_tile.deselect()
+        self.current_tile = self.grid[coordinates]
+        self.current_tile.select()
 
 
-
-class Game():
+class Game:
 
     def __init__(self):
         self.windowSurface = pygame.display.set_mode((WINDOW_HEIGHT, WINDOW_WIDTH), 0, 32)
@@ -62,23 +72,22 @@ class Game():
         basic_font = pygame.font.SysFont(None, 48)
         self.windowSurface.fill(WHITE)
 
-        self.grid.draw_grid_lines(self.windowSurface)
         pygame.display.update()
 
         # game loop
         while True:
             pygame.display.update()
-            # user event handling
             for event in pygame.event.get():
                 if event.type == QUIT:
                     pygame.quit()
                     sys.exit()
                 if event.type == MOUSEBUTTONUP:
-                    xpos = event.pos[0]//SQUARE_SIZE
-                    ypos = event.pos[1]//SQUARE_SIZE
-                    self.grid.select_tile((xpos, ypos))
+                    if event.pos[0] < SQUARE_SIZE*HOR_SQUARES:
+                        xpos = event.pos[0]//SQUARE_SIZE
+                        ypos = event.pos[1]//SQUARE_SIZE
+                        self.grid.select_tile((xpos, ypos))
             self.grid.draw(self.windowSurface)
 
 
 if __name__ == '__main__':
-	Game().run()
+    Game().run()
